6ead7b899d5befc372f80844409e8cf4f6a8f68a
Sam <samuel.thomas.wagner@gmail.com>
2020-01-25 23:06:01 -0700

    edited

--- web/assignments.php
+++ web/assignments.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en-US"> 
     <?php include 'shared/header.php';?>
-    <h1><a href="/week3/assignment/addItems.php"></a></h1>
+    <h1><a href="../web/week3/assignment/addItems.php"></a></h1>
     <?php include 'shared/footer.php';?>
 </html>
