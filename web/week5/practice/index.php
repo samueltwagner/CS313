9c5e5243003a099bf369485e044a280215a4c75e
Sam <samuel.thomas.wagner@gmail.com>
2020-02-04 16:15:52 -0700

    test

--- web/week5/practice/index.php
+++ web/week5/practice/index.php
@@ -1,27 +1,21 @@
 <?php
-    require "dbConnect.php";
-    $db = get_db();
-
-    // $family_members = $db->prepare("SELECT * FROM w5_family_members");
-    // $family_members->execute();
-
-    //it worked
-
-    // while ($fRow = $family_members->fetch(PDO::FETCH_ASSOC))
-    // {
-    //     $first_name = $fRow["first_name"];
-    //     $last_name = $fRow["last_name"];
-    //     $relationship_id = $fRow["relationship_id"];
-    //     //it worked
-    //     $relationships = $db->prepare("SELECT description FROM w5_relationships WHERE id = $relationship_id");
-    //     $relationships->execute();
-    //     while ($rRow = $relationships->fetch(PDO::FETCH_ASSOC))
-    //     {
-    //         $relationship = $rRow["description"];
-    //     }
-
-    //     echo "<p>$first_name $last_name is my $relationship ($relationship_id)</p>";
-    // }
-
+   require "dbConnect.php";
+   $db = get_db();
+   $family_members = $db->prepare("SELECT * FROM w5_family_members");
+   $family_members->execute();
+   while ($fRow = $family_members->fetch(PDO::FETCH_ASSOC))
+   {
+      $first_name = $fRow["first_name"];
+      $last_name = $fRow["last_name"];
+      $relationship_id = $fRow["relationship_id"];
+      $relationships = $db->prepare("SELECT description FROM w5_relationships WHERE id = $relationship_id");
+      $relationships->execute();
+      while ($rRow = $relationships->fetch(PDO::FETCH_ASSOC))
+      {
+         $relationship = $rRow["description"];
+      }
+      echo "<p>$first_name $last_name is my $relationship ($relationship_id)</p>";
+   }
 ?>
+
 <h1>hello 2</h1>
