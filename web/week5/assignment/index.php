9472bf50e51e43583c6c0bee285ca48def1d9e09
Sam <samuel.thomas.wagner@gmail.com>
2020-02-08 21:03:39 -0700

    week 5

--- web/week5/assignment/index.php
+++ web/week5/assignment/index.php
@@ -3,19 +3,18 @@
    $db = get_db();
    $workoutDetails = $db->prepare("SELECT * FROM p90x_workout_details");
    $workoutDetails->execute();
-   while ($wRow = $workoutDetails->fetch(PDO::FETCH_ASSOC))
-   {
-      $workout_title = $wRow["workout_title"];
-      $exercise_id = $wRow["exercise_id"];
-      $exercises = $db->prepare("SELECT exercise_title FROM p90x_exercises WHERE id = $exercise_id");
-      $exercises->execute();
-      while ($wRow = $exercises->fetch(PDO::FETCH_ASSOC))
-      {
-         $exercise_title = $wRow["exercise_title"];
-      }
-      echo "<h1>Your workout today is $workout_title.</h1>";
-      echo "<h2>Exercise is $exercise_title ($exercise_id)</h2>";
-   }
+   $wRow = $workoutDetails->fetch(PDO::FETCH_ASSOC);
+   
+   $workout_title = $wRow["workout_title"];
+   $exercise_id = $wRow["exercise_id"];
+   $exercises = $db->prepare("SELECT exercise_title FROM p90x_exercises WHERE id = $exercise_id");
+   $exercises->execute();
+   
+   $wRow = $exercises->fetch(PDO::FETCH_ASSOC);
+   $exercise_title = $wRow["exercise_title"];
+
+   echo "<h1>Your workout today is $workout_title.</h1>";
+   echo "<h2>Exercise is $exercise_title ($exercise_id)</h2>";
    echo "<h1>Where the magic happens</h1>"
 ?>
 
