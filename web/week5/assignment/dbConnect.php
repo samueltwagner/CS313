64f38e8dad1f4e66b4a255551854561b31215e31
Sam <samuel.thomas.wagner@gmail.com>
2020-02-08 21:53:39 -0700

    week 5

--- web/week5/assignment/dbConnect.php
+++ web/week5/assignment/dbConnect.php
@@ -1,4 +1,4 @@
-<!-- heroku config -s -->
+<!-- Top of dbConnect.php-->
 <?php
 
 function get_db(){
@@ -28,19 +28,24 @@ function get_db(){
   echo "<h1>Database is Connected!</h1>";
   echo "<h1>this php statement is working!</h1>";
   echo "<h1>Where the magic happens</h1>";
+  echo "<h1>this is the SQL Statment $workoutDetails";
 
-  $workoutDetails = $db->prepare("SELECT * FROM p90x_workout_details");
+  $workoutDetails = $db->prepare("SELECT workout_title FROM p90x_workout_details");
   $workoutDetails->execute();
-  $wRow = $workoutDetails->fetch(PDO::FETCH_ASSOC);
+  while ($wRow = $workoutDetails->fetch(PDO::FETCH_ASSOC))
+  {
+    print_r($workoutDetails);
+     $workout_title = $wRow["workout_title"];
+     $exercise_id = $wRow["exercise_id"];
+     $exercises = $db->prepare("SELECT * FROM p90x_exercises WHERE id = $exercise_id");
+     $exercises->execute();
+     while ($wRow = $exercises->fetch(PDO::FETCH_ASSOC))
+     {
+        $exercise_title = $wRow["exercise_title"];
+     }
+     echo "<h1>Your workout today is $workout_title.</h1>";
+     echo "<h2>Exercise is $exercise_title ($exercise_id)</h2>";
+  }
   
-  $workout_title = $wRow["workout_title"];
-  $exercise_id = $wRow["exercise_id"];
-  $exercises = $db->prepare("SELECT exercise_title FROM p90x_exercises WHERE id = $exercise_id");
-  $exercises->execute();
-  
-  $wRow = $exercises->fetch(PDO::FETCH_ASSOC);
-  $exercise_title = $wRow["exercise_title"];
 
-  echo "<h1>Your workout today is $workout_title.</h1>";
-  echo "<h2>Exercise is $exercise_title ($exercise_id)</h2>";
 ?>
--- web/week5/assignment/index.php
+++ web/week5/assignment/index.php
@@ -1,3 +1,5 @@
+<!-- Top of index.php-->
+<h1>Top of Index.php<h1>
 <?php
    require "dbConnect.php";
    $db = get_db();
@@ -9,25 +11,26 @@
    $exercise_id = $wRow["exercise_id"];
    $exercises = $db->prepare("SELECT exercise_title FROM p90x_exercises WHERE id = $exercise_id");
    $exercises->execute();
-   
+
    $wRow = $exercises->fetch(PDO::FETCH_ASSOC);
    $exercise_title = $wRow["exercise_title"];
 
    echo "<h1>Your workout today is $workout_title.</h1>";
    echo "<h2>Exercise is $exercise_title ($exercise_id)</h2>";
-   echo "<h1>Where the magic happens</h1>"
+   echo "<h1>Inside the first php statement.</h1>";
 ?>
-
+<br>
+<h1>Below the first php statement</h1>
+<br>
+<h1>Above the second php statment</h1>
 <?php
       $statement = $db->prepare("SELECT * FROM p90x_workout_title");
       $statement->execute();
       // Go through each result
       while ($row = $statement->fetch(PDO::FETCH_ASSOC))
       {
-         // The variable "row" now holds the complete record for that
-         // row, and we can access the different values based on their
-         // name
          $workout_title2 = $row['workout_title'];
-         echo "<p>$workout_title2<p>";
+         echo "<p>This is the workout title $workout_title2<p>";
+         echo "<h1>Inside the second php statement.</h1>";
       }
 ?>
