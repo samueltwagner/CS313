0124bfeaa56b950d60e5a7ff8608eeb06f80bdaa
Sam <samuel.thomas.wagner@gmail.com>
2020-02-08 21:04:57 -0700

    week 5

--- web/week5/assignment/dbConnect.php
+++ web/week5/assignment/dbConnect.php
@@ -28,4 +28,20 @@ function get_db(){
   echo "<h1>Database is Connected!</h1>";
   echo "<h1>this php statement is working!</h1>";
   echo "<h1>Where the magic happens</h1>";
+
+  $workoutDetails = $db->prepare("SELECT * FROM p90x_workout_details");
+  $workoutDetails->execute();
+  $wRow = $workoutDetails->fetch(PDO::FETCH_ASSOC);
+  
+  $workout_title = $wRow["workout_title"];
+  $exercise_id = $wRow["exercise_id"];
+  $exercises = $db->prepare("SELECT exercise_title FROM p90x_exercises WHERE id = $exercise_id");
+  $exercises->execute();
+  
+  $wRow = $exercises->fetch(PDO::FETCH_ASSOC);
+  $exercise_title = $wRow["exercise_title"];
+
+  echo "<h1>Your workout today is $workout_title.</h1>";
+  echo "<h2>Exercise is $exercise_title ($exercise_id)</h2>";
+  echo "<h1>Where the magic happens</h1>"
 ?>
