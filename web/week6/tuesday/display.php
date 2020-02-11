c06e04c7fc2936ccdfbd2b7b3b19960d858ffc29
Sam <samuel.thomas.wagner@gmail.com>
2020-02-11 15:44:18 -0700

    test

--- web/week6/tuesday/display.php
+++ web/week6/tuesday/display.php
@@ -5,10 +5,28 @@
 	<body>
 		<div class="container">
          <?php
-            // retrieve url parameter
-            // execute query to pull up data from that id
-            // execute another query to get food data
-            // display name and favorite food
+         $personId = $_GET['personId'];
+         $statement = $db->prepare('SELECT * FROM w6_user WHERE ID = :personId');
+         $statement->bindValue(':personId', $personId);
+         $statement->execute();
+         while ($row = $statment->fetch(PDO::FETCH_ASSOC))
+         {
+
+            $id = $row['id'];
+            $first = $row['first'];
+            $last = $row['last'];
+            $food_id = $row['food_type'];
+
+            $statement = $db->prepare('SELECT food FROM w6_food WHERE ID = $food_Id');
+            $statement->execute();
+
+            while ($row = $statment->fetch(PDO::FETCH_ASSOC))
+            {
+               $food = $fRow['food'];
+            }
+
+            echo "<h1>$first $last's favorite food is $food</h1>";
+         }
          ?>
 
 		</div>
