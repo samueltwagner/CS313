514c9a6d47d71235d759c3a264ff4d9d2553ebd0
Sam <samuel.thomas.wagner@gmail.com>
2020-02-11 15:36:55 -0700

    test

--- web/week6/tuesday/insert.php
+++ web/week6/tuesday/insert.php
@@ -18,6 +18,7 @@ try
 
 	// SELECT c.relname FROM pg_class c WHERE c.relkind = 'S';   -- display all sequences
 	// get id of last inserted row - save in $userId
+	$userId = $db->lastInsertId("w6_user_id_seq");
 }
 catch (Exception $ex)
 {
