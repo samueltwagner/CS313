e33329c42b208d2fed8ea404273664bdb1b073af
Sam <samuel.thomas.wagner@gmail.com>
2020-02-22 19:55:26 -0700

    config file changes

--- web/week7/p90x-php/index.php
+++ web/week7/p90x-php/index.php
@@ -2,10 +2,7 @@
 
 
 <?php
-phpinfo();
-// ini_set('display_startup_errors', true);
-// error_reporting(E_ALL);
-// ini_set('display_errors', true);
+// phpinfo();
 
 require_once(__DIR__ . '/library/main.php');
 
