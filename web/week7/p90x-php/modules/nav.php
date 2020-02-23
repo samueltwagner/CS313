692572aa140fd2775a3aafac1463c265f3413737
Sam <samuel.thomas.wagner@gmail.com>
2020-02-22 23:50:01 -0700

    week 7

--- web/week7/p90x-php/modules/nav.php
+++ web/week7/p90x-php/modules/nav.php
@@ -1,13 +1,13 @@
 <div id="nav" class="nav">
 	<div class="text" class="icon">
 		<ul>
-				<li><a class="link" title="Home" href="?page=home"><i class="fa fa-home "></i></a><a class="link" title="Home" href="?page=home">Home</a></li>
+				<li><a class="link" title="Home" href="?page=home"><i class="fa fa-home "></i> Home</a></li>
         <?php 
           if ($auth->isLogged()) {
             ?>
-              <li><a class="link" href="?page=workouts"><i class="fa fa-edit "></i>Workouts</a></li>
+              <li><a class="link" href="?page=workouts"><i class="fa fa-edit "></i> Workouts</a></li>
               <!-- <li><a href="?page=categories" >Categories</a></li> -->
-              <li><a class="link" href="?logout"><i class="fa fa-sign-out"></i></a><a class="link" href="?logout">Logout</a></li>
+              <li><a class="link" href="?logout"><i class="fa fa-sign-out"></i> Logout</a></li>
             <?php
           }
         ?>
